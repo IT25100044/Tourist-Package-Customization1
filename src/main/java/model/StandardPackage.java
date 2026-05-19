d65414c4d0260e2ba70ce32957be9de6bfb28922
Naveen <IT25101155@my.sliit.lk>
2026-05-19 18:59:50 +0530

    Delete src/main/java directory

--- src/main/java/model/StandardPackage.java
+++
@@ -1,6 +0,0 @@
-package model;
-public class StandardPackage extends TravelPackage{
- public StandardPackage(String id,String name,String destination,int duration,double basePrice,double discount,String description){super(id,name,destination,duration,basePrice,discount,description,"STANDARD");}
- @Override public double calculatePrice(){return Math.max(0,getBasePrice()-getDiscount());}
- public String toFileString(){return getId()+","+getName()+","+getDestination()+","+getDuration()+","+getBasePrice()+","+getDiscount()+","+getDescription().replace(","," ");}
-}
